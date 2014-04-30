66531c102df5feca24aa4edcb67d0281bb96d283
akDeveloper <andreas.kollaros@gmail.com>
2014-04-30 20:41:05 +0300

    fix test init

--- test/test_init.php
+++ test/test_init.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'larium_database/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/Config.php';
 require_once __DIR__ . '/Model/Author.php';
 require_once __DIR__ . '/Model/Book.php';
